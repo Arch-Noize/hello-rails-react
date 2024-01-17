02837ab48d967175c617515ddea9bd1bfef84e71
Arch-Noize <santiagonoize@gmail.com>
2024-01-17 07:10:37 -0400

    Add API

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  root 'root#index'
+  namespace :api do
+    get '/random_greeting', to: 'greetings#random'
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,5 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+Greeting.create(message: "Welcome to our App!")
+Greeting.create(message: "Bienvenid@ a nuestra App!")
+Greeting.create(message: "You ever wondered, what's going inside their head?")
+Greeting.create(message: "Now playing: Laplace's Angel - Will Wood")
+Greeting.create(message: "The trees deny themselves nothing that makes them grow")
